bf6da778bbf9493259377bdcccc5935b8c65e4df
Egor <egork2205@gmail.com>
2025-04-27 15:44:19 +0700

    Задание 5.2.8

--- TasksForModul5/Program.cs
+++ TasksForModul5/Program.cs
@@ -10,70 +10,39 @@ namespace TasksForModul5
     {
         static void Main(string[] args)
         {
-            var (name, age) = ("Евгения", 27);
+           int[] array = GetArrayFromCosole();
+           array = SortArray(array);
+        }
+        static int[] GetArrayFromCosole()
+        {
+            var result = new int[5];
 
-            Console.WriteLine("Мое имя: {0}", name);
-            Console.WriteLine("Мой возраст: {0}", age);
-
-            Console.Write("Введите имя: ");
-            name = Console.ReadLine();
-            Console.Write("Введите возраст с цифрами:");
-            age = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Ваше имя: {0}", name);
-            Console.WriteLine("Ваш возраст: {0}", age);
-
-            var favcolor = new string[3];
-
-            for (int i = 0; i < favcolor.Length; i++)
+            for (int i = 0; i < result.Length; i++)
             {
-                favcolor[i] = ShowColor(name, age);
-
+                Console.Write($"Введите число номер {i + 1} масива: ");
+                result[i] = int.Parse(Console.ReadLine());
             }
-
-            Console.WriteLine("Ваши любимые цвета: ");
-            foreach (var color in favcolor)
-            {
-                Console.WriteLine(color);
-            }
+            return result;
+           
 
         }
-
-        static string ShowColor(string username, int userage)
+        static int [] SortArray(int[] result)
         {
-            Console.WriteLine($"{username},{userage} лет\nНапишите свой любимый цвет на английском с маленькой буквы");
-            var color = Console.ReadLine();
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
 
-            switch (color)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                    }
 
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    break;
-
-            }
-            return color;
+            for (int i = 0; i < result.Length; i++)
+                Console.WriteLine(result[i]);
+            return result;
         }
     }
+
 }
