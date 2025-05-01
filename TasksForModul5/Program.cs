23c829fba4d690b05ebebe57f0a1b5bebc929b14
Egor <egork2205@gmail.com>
2025-05-01 22:05:58 +0700

    Задание 5.5.3

--- TasksForModul5/Program.cs
+++ TasksForModul5/Program.cs
@@ -23,10 +23,18 @@ namespace TasksForModul5
         }
         static void Echo(string phrase, int deep)
         {
-            Console.WriteLine(phrase);
+            string modif = phrase;
+
+            if (modif.Length > 2)
+            { 
+            modif = modif.Remove(0, 2);
+                Console.WriteLine($"...{modif}" );
+            }
+
+            
             if (deep > 1)
             {
-                Echo (phrase, deep - 1);
+                Echo (modif, deep - 1);
             }
                 
         }
