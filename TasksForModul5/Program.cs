0c0e79ca7edc0b4e00ead98d4c82bebfc4a6fa8c
Egor <egork2205@gmail.com>
2025-05-01 22:19:55 +0700

    Программа вычисления произведения числа и факториала

--- TasksForModul5/Program.cs
+++ TasksForModul5/Program.cs
@@ -11,35 +11,20 @@ namespace TasksForModul5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите что-то...");
-
-            var str = Console.ReadLine();
-
-            Console.WriteLine("Укажите глубину эха");
-
-            var deep = int.Parse(Console.ReadLine());
-
-            Echo(str, deep);
+            int x = int.Parse(Console.ReadLine());
+            decimal res = Factorial(x);
+            Console.WriteLine(res);
         }
-        static void Echo(string phrase, int deep)
+        static decimal Factorial(int x)
         {
-            string modif = phrase;
-
-            if (modif.Length > 2)
-            { 
-            modif = modif.Remove(0, 2);
-                 
-            }
-            Console.BackgroundColor = (ConsoleColor)deep;
-            Console.WriteLine($"...{modif}");
-
-
-            if (deep > 1)
+            if (x == 0)
             {
-                Echo (modif, deep - 1);
+                return 1;
             }
-                
+            else
+            {
+                return x * Factorial(x - 1);
+            }
         }
-           
     }
 }
