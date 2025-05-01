9371ffc0f1c319c847d01e74106a1ff1ef103c19
Egor <egork2205@gmail.com>
2025-05-01 19:04:30 +0700

    Задание 5.3.13

--- TasksForModul5/Program.cs
+++ TasksForModul5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,70 @@ namespace TasksForModul5
     {
         static void Main(string[] args)
         {
-            int size = 9;
-            int[] array = new int[size]; 
-            
-            GetArrayFromConsole(array, ref size);
-            
-        }
-       static int [] GetArrayFromConsole(int [] array, ref int size)
-        {
-            size = 6;
-            int [] newarray = new int[size];
+            int[] array = new int[] { -48, 89, -9, 225, -58, -2, 199 };
+            int[] sortedDesc;
+            int[] sortedAsc;
 
-            for (int i = 0; i < size; i++)
+            SortArray(array, out sortedDesc, out sortedAsc);
+
+            Console.WriteLine("Исходный массив:");
+            foreach (int element in array)
             {
-                Console.Write($"Введите число номер {i + 1}: ");
-                newarray[i] = int.Parse(Console.ReadLine());
+                Console.Write(element + " ");
             }
-            return newarray;
+            Console.WriteLine();
+
+            Console.WriteLine("Отсортированный по возрастанию:");
+            foreach (int element in sortedAsc)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Отсортированный по убыванию:");
+            foreach (int element in sortedDesc)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void SortArray(in int[] array, out int[] sortedDesc, out int[] sortedAsc)
+        {
+            sortedDesc = SortArrayDesc(array);
+            sortedAsc = SortArrayAsc(array);
+        }
+
+        static int[] SortArrayAsc(in int[] array)
+        {
+           
+            int[] result = (int[])array.Clone();
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+            return result;
+        }
+
+        static int[] SortArrayDesc(in int[] array)
+        {
+           
+            int[] result = (int[])array.Clone();
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] < result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+            return result;
         }
     }
 
